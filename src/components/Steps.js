dc16da845292a096aff6ed31279335891aa7ef88
Ignacio Streuly <ijstreuly@gmail.com>
2018-04-21 18:11:18 -0400

    Add: Orbit animation tweaks

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -34,7 +34,7 @@ class Steps extends Component {
     addAnimationClass () {
         setTimeout(this.triggerOrbitAnimation, 0);
         // Orbit animation has a duration of 3000
-        setTimeout(this.triggerTextAnimation, 2500);
+        setTimeout(this.triggerTextAnimation, 3500);
     }
 
     triggerOrbitAnimation () {
@@ -109,31 +109,31 @@ class Steps extends Component {
 
     render () {
         return (
-            <div className="steps-component">
+            <Waypoint onEnter={this.addAnimationClass}>
+                <div className="steps-component">
+                    {/* Hide on mobile */}
+                    <div className="step-text-wrapper step-one-wrapper">
+                        <span className={'step-header ' + this.state.stepTextHeaderOpacity}>No-Risk Growth Workshop</span>
+                        <p className={'step-text ' + this.state.stepTextSubHeaderOpacity1}>
+                            We'll start out with a call to get a feel for your current business goals, marketing stack, and analytics setup.
+                        </p>
+                    </div>
+                    <div className="step-text-wrapper step-two-wrapper">
+                        <span className={'step-header ' + this.state.stepTextHeaderOpacity}>The Growth Report</span>
+                        <p className={'step-text step-text-two ' + this.state.stepTextSubHeaderOpacity2}>
+                            We'll take all of the learnings from our growth workshop and send you an actionable growth report.
+                        </p>
+                    </div>
 
-                {/* Hide on mobile */}
-                <div className="step-text-wrapper step-one-wrapper">
-                    <span className={'step-header ' + this.state.stepTextHeaderOpacity}>No-Risk Growth Workshop</span>
-                    <p className={'step-text ' + this.state.stepTextSubHeaderOpacity1}>
-                        We'll start out with a call to get a feel for your current business goals, marketing stack, and analytics setup.
-                    </p>
-                </div>
-                <div className="step-text-wrapper step-two-wrapper">
-                    <span className={'step-header ' + this.state.stepTextHeaderOpacity}>The Growth Report</span>
-                    <p className={'step-text step-text-two ' + this.state.stepTextSubHeaderOpacity2}>
-                        We'll take all of the learnings from our growth workshop and send you an actionable growth report.
-                    </p>
-                </div>
+                    <div className="step-text-wrapper step-three-wrapper">
+                        <span className={'step-header ' + this.state.stepTextHeaderOpacity}>Executing on Growth</span>
+                        <p className={'step-text ' + this.state.stepTextSubHeaderOpacity3}>
+                            You can either attempt to implement the Growth Report internally or we, then, discuss how we can
+                            help execute the most pressing opportunities in an exploratory kickoff call.
+                        </p>
+                    </div>
 
-                <div className="step-text-wrapper step-three-wrapper">
-                    <span className={'step-header ' + this.state.stepTextHeaderOpacity}>Executing on Growth</span>
-                    <p className={'step-text ' + this.state.stepTextSubHeaderOpacity3}>
-                        You can either attempt to implement the Growth Report internally or we, then, discuss how we can
-                        help execute the most pressing opportunities in an exploratory kickoff call.
-                    </p>
-                </div>
 
-                <Waypoint onEnter={this.addAnimationClass}>
                     <div className={'outer-circle ' + this.state.outerCircleAnimationClass}>
                         <div onClick={() => this.showStepSubText(1)} id={'stepOneCircle'} className={'step-circle step-one ' + this.state.stepAnimationClass}>
                             <p className="step-number">1</p>
@@ -147,18 +147,17 @@ class Steps extends Component {
                             <p className="step-number">3</p>
                         </div>
                     </div>
-                </Waypoint>
 
-                <div className="middle-circle">
-                    <div className="center-circle ">
-                        <div className="center-header-wrap">
-                            <h2 className="center-circle-header">Less Hassle.</h2>
-                            <h2 className="center-circle-header">Less BS.</h2>
+                    <div className="middle-circle">
+                        <div className="center-circle ">
+                            <div className="center-header-wrap">
+                                <h2 className="center-circle-header">Less Hassle.</h2>
+                                <h2 className="center-circle-header">Less BS.</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-            </div>
+            </Waypoint>
         )
     }
 }
